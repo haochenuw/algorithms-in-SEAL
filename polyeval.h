7b3abbbf5a272c056875f73908f5c738f49b6bac
hao chen <chenh123@uw.edu>
2019-11-25 20:19:10 -0800

    added files

---
+++ polyeval.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <cstddef>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <random>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <limits>
+#include <algorithm>
+#include <numeric>
+#include "seal/seal.h"
+
+/*
+Helper function: Prints the parameters in a SEALContext.
+*/
+inline void print_parameters(std::shared_ptr<seal::SEALContext> context)
+{
+    // Verify parameters
+    if (!context)
+    {
+        throw std::invalid_argument("context is not set");
+    }
+    auto &context_data = *context->key_context_data();
+
+    /*
+    Which scheme are we using?
+    */
+    std::string scheme_name;
+    switch (context_data.parms().scheme())
+    {
+    case seal::scheme_type::BFV:
+        scheme_name = "BFV";
+        break;
+    case seal::scheme_type::CKKS:
+        scheme_name = "CKKS";
+        break;
+    default:
+        throw std::invalid_argument("unsupported scheme");
+    }
+    std::cout << "/" << std::endl;
+    std::cout << "| Encryption parameters :" << std::endl;
+    std::cout << "|   scheme: " << scheme_name << std::endl;
+    std::cout << "|   poly_modulus_degree: " <<
+        context_data.parms().poly_modulus_degree() << std::endl;
+
+    /*
+    Print the size of the true (product) coefficient modulus.
+    */
+    std::cout << "|   coeff_modulus size: ";
+    std::cout << context_data.total_coeff_modulus_bit_count() << " (";
+    auto coeff_modulus = context_data.parms().coeff_modulus();
+    std::size_t coeff_mod_count = coeff_modulus.size();
+    for (std::size_t i = 0; i < coeff_mod_count - 1; i++)
+    {
+        std::cout << coeff_modulus[i].bit_count() << " + ";
+    }
+    std::cout << coeff_modulus.back().bit_count();
+    std::cout << ") bits" << std::endl;
+
+    /*
+    For the BFV scheme print the plain_modulus parameter.
+    */
+    if (context_data.parms().scheme() == seal::scheme_type::BFV)
+    {
+        std::cout << "|   plain_modulus: " << context_data.
+            parms().plain_modulus().value() << std::endl;
+    }
+
+    std::cout << "\\" << std::endl;
+}
+
+void example_polyeval();
+
---
+++ polyeval_main.cpp
@@ -0,0 +1,13 @@
+#include "polyeval.h"
+
+using namespace std;
+using namespace seal;
+
+
+
+int main(){
+	example_polyeval();
+
+
+	return 0;
+}
